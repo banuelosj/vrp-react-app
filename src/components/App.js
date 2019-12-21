3a187e0cf035346e1136b75519a13355057eb970
Jose Banuelos <JBanuelos@esri.com>
2019-12-21 12:19:21 -0800

    added the VRPServiceButton

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import Menu from './Menu';
+import VRPServiceButton from './VRPServiceButton';
 
-const App = () => {
-    return (
-        <div className="pusher">
-            <div className="ui inverted vertical masthead center aligned segment">
-                <Menu />
-                <br />
-                <br />
-                <div className="ui text container" style={{padding: '20px'}}>
-                    <h1 className="ui inverted header">
-                        Vehicle Routing Problem Tester
-                    </h1>
+class App extends React.Component {
+    state = {logisticsSelected: false};
+
+    render() {
+        return (
+            <div className="pusher">
+                <div className="ui inverted segment">
+                    <div className="ui inverted vertical masthead center aligned segment">
+                        <Menu />
+                        <br />
+                        <br />
+                        <div className="ui text container" style={{padding: '20px'}}>
+                            <h1 className="ui inverted header">
+                                Vehicle Routing Problem Tester
+                            </h1>
+                        </div>
+                    </div>
+                    <VRPServiceButton />
+                </div>
+                <div className="ui vertical stripe segment">
+                    Form
                 </div>
             </div>
-            <div className="ui vertical stripe segment">
-                Form
-            </div>
-        </div>
-    );
+        );
+    } //end of render()
 }
 
 export default App;
---
+++ src/components/VRPServiceButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const VRPServiceButton = (props) => {
+    return (
+        <div className="ui inverted segment">
+            <div class="ui buttons">
+                <button class="ui button">sampleserver6</button>
+                <div class="or"></div>
+                <button class="ui positive button">logistics.arcgis.com</button>
+            </div>
+        </div>
+    );
+}
+
+export default VRPServiceButton;
